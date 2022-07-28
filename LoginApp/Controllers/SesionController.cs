2871d193adb56ff5a343c2113e62199321f38122
Teal321Uva <aldoteran321@gmail.com>
2022-07-28 10:51:59 -0500

    subiendo los primeros archivos

---
+++ LoginApp/Controllers/SesionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class SesionController : Controller
+    {
+        // GET: Sesion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+    }
+}
